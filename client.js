e84e1a5171c2103650f827a5f67da0da318602d2
Garey Salinas <gareytwin1@gmail.com>
2024-11-25 21:41:36 -0600

    enhance WebSocket client: emit test message on connect and improve message logging

--- client.js
+++ client.js
@@ -4,10 +4,12 @@ const socket = io("ws://127.0.0.1:5000");
 
 socket.on("connect", () => {
     console.log("Connected to the server");
+    // Emit a test message
+    socket.emit("message", "Hello from the client");
 });
 
 socket.on("message", (data) => {
-    console.log("Received message:", data);
+    console.log("Received message:", data.response || data);
 });
 
 socket.on("disconnect", () => {
